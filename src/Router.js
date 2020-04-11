fbf817ef59c4863aaf7af726effa92ac678eddb1
AndrewSimons3 <simdrew07@gmail.com>
2020-04-11 17:40:39 -0500

    finished

--- src/Router.js
+++ src/Router.js
@@ -12,6 +12,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router'
 import Home from './components/Home'
 import About from './components/About'
+import Car from './components/Car'
 // Write component imports here //
 
 
@@ -23,6 +24,7 @@ function Router() {
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/About" component={About} />
+            <Route path="/car/:id" component={Car} />
         </Switch>
         </div>
     );
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,46 @@
 import React from 'react'
 import cars from '../cars.json'
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
+import { makeStyles } from '@material-ui/core/styles';
 // import material ui components here //
 // Container, Paper, Chip //
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      '& > *': {
+        margin: theme.spacing(1),
+        width: theme.spacing(16),
+        height: theme.spacing(16),
+      },
+    },
+  }));
+
 const Car = (props) => {
+    const classes = useStyles();
+
+    const id = props.match.params.id
+    console.log(props.match.params)
+    const findCars = cars[id]
     return (
-        <h1>A specific car</h1>
+        // <div className={classes.root}>
+        <Container fixed>
+            <Paper elevation={3} >
+                <h2>{findCars.Name}</h2> 
+                <Chip label={`id: ${findCars.id}`} />
+                <Chip label={`Miles_per_Gallon: ${findCars.Miles_per_Gallon}`} /> 
+                <Chip label={`Cylinders: ${findCars.Cylinders}`} /> 
+                <Chip label={`Displacement: ${findCars.Displacement}`} /> 
+                <Chip label={`Horsepower: ${findCars.Horsepower}`} /> 
+                <Chip label={`Weight_in_lbs: ${findCars.Weight_in_lbs}`} /> 
+                <Chip label={`Acceleration: ${findCars.Acceleration}`} />    
+                <Chip label={`Year: ${findCars.Year}`} /> 
+                <Chip label={`Origin: ${findCars.Origin}`} />      
+            </Paper>
+        </Container>
     )
 }
 
